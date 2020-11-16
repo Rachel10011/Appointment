1165cb735f0fd997f336551c778315878f58aa2c
TraLe <letra1712@gmail.com>
2020-11-16 12:08:22 -0500

    Setup

---
+++ Project1/Features.c
@@ -0,0 +1,8 @@
+//PROG71985F20 - Group Assignment
+//Fall 2020
+//Group members:
+	//Thi Huong Tra Le
+	//Dominic Pham
+	//Nguyen Le Minh
+
+#include "Features.h"
--- Project1/Features.h
+++ Project1/Features.h
@@ -1,1 +1,22 @@
+//PROG71985F20 - Group Assignment
+//Fall 2020
+//Group members:
+	//Thi Huong Tra Le
+	//Dominic Pham
+	//Nguyen Le Minh
+
 #pragma once
+#include <stdio.h>
+
+//Required features:
+void addNewAppt();
+void deleteExistingAppt();
+void updateExistingAppt();
+void displaySingleAppt();
+void displayRangeAppt();
+void displayAllAppt();
+void searchForAppt();
+
+//Friendly/easy to use UI (or menu system) --- nice and clean design?
+//save accumulated data to disk   ---- save to file?
+//load accumulated data from disk ---- load data from file?
