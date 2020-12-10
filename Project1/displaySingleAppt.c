14dadd8f5df5459d659bfd1ed532daa8049c6dfa
DPham <dom17pham@gmail.com>
2020-12-10 11:20:18 -0500

    fix displaySingleAppt and updatingExistingAppt.

--- Project1/displaySingleAppt.c
+++ Project1/displaySingleAppt.c
@@ -8,16 +8,19 @@ void displaySingleAppt(PAPPOINTMENT apptList[], int* size) {
 	printf("Enter the name of the appointment you wish to display: ");
 	(void)scanf_s("%s", &ApptName);
 
-	for (int i = 0; i < size; i++)
-	{
-		if (strcmp(ApptName, apptList[i]->apptName) == 0) {
-			printf("Appointment name: %s\n", apptList[i]->apptName);
-			printf("Location: %s\n", apptList[i]->location);
-			printf("Starts at %d:%d\n", apptList[i]->startHour, apptList[i]->startMinutes);
-			printf("Ends at %d:%d\n", apptList[i]->endHour, apptList[i]->endMinutes);
-			printf("Description: %s\n", apptList[i]->body);
+	if (size != NULL) {
+		for (int i = 0; i < size; i++)
+		{
+			if (strcmp(ApptName, apptList[i]->apptName) == 0) {
+				printf("Appointment name: %s\n", apptList[i]->apptName);
+				printf("Location: %s\n", apptList[i]->location);
+				printf("Starts at %d:%d\n", apptList[i]->startHour, apptList[i]->startMinutes);
+				printf("Ends at %d:%d\n", apptList[i]->endHour, apptList[i]->endMinutes);
+				printf("Description: %s\n", apptList[i]->body);
+			}
 		}
 	}
-}/* Minh Le: I think you should check the apptlist if it NULL before the for loop 
- And you should display all the appt to the user to choose cuz they maybe forget thier name appointment
- */
+	else {
+		printf("There are no appointments to display.");
+	}
+}
