ecf21caa432d296bf1e7ce8ef22606c7a26033bf
DPham <dom17pham@gmail.com>
2020-12-11 15:31:46 -0500

    created .c file for displayRangeAppt

--- Project1/Features.c
+++ Project1/Features.c
@@ -177,48 +177,6 @@ void disposeAppt(PAPPOINTMENT appt[], int* size)
 }
 
 
-void displayRangeAppt(PAPPOINTMENT apptList[], int* size)
-
-{
-	if (*size == NULL)
-		return;
-	
-	displayAllAppt(apptList, size);
-
-	for (int i = 0; i < *size; i++)
-	{
-
-		int range_min, range_hour;
-		
-		i=getUserInput("Enter the number of the appointment you want to check the range:");
-
-		range_min = apptList[i]->endMinutes - apptList[i]->startMinutes;
-		
-		if (range_min < 0)
-		{
-			int endhour = apptList[i]->endHour - 1;
-			int endminute = apptList[i]->endMinutes + 60;
-			int rangeMin = endminute - apptList[i]->startMinutes;
-			range_hour = endhour - apptList[i]->startHour;
-			if (range_hour < 0)
-			{
-				printf("Invalid Appointment");
-			}
-			else
-			{
-				printf("The range of the appointment : %d:%d", range_hour, rangeMin);
-			}
-			return;
-		}
-		else
-			range_hour = apptList[i]->endHour - apptList[i]->startHour;
-
-		printf("The range of the appointment %d:%d-%d:%d is %d:%d", apptList[i]->startHour, apptList[i]->startMinutes, apptList[i]->endHour, apptList[i]->endMinutes,range_hour, range_min);
-		return;
-	}
-
-}
-
 void askForFilePath(char fileName[])
 {
 	FILE* fp;
